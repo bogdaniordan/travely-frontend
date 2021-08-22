f79bb228f4637932d38dab78da99b1f27ef6ea9e
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-08-22 12:43:01 +0300

    Added sign up validations

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -94,17 +94,11 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-    },
+
+},
     avatar: {
         margin: theme.spacing(1),
         backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%',
-        marginTop: theme.spacing(3),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
     },
 }));
 
@@ -127,33 +121,72 @@ const Register = () => {
     }
 
     const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [username, setUsername] = useState();
+    const [gender, setGender] = useState();
+    const [phoneNumber, setPhoneNumber] = useState();
+    const [address, setAddress] = useState();
+    const [age, setAge] = useState();
+
+    const [successful, setSuccessful] = useState(false);
+    const [message, setMessage] = useState("");
 
     const onChangeFirstName = event => {
         setFirstName(event.target.value)
     }
 
-    const [lastName, setLastName] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [successful, setSuccessful] = useState(false);
-    const [message, setMessage] = useState("");
+    const onChangeLastName = event => {
+        setLastName(event.target.value)
+    }
+
+    const onChangeEmail = event => {
+        setEmail(event.target.value)
+    }
+
+    const onChangeUsername = event => {
+        setUsername(event.target.value)
+    }
+
+    const onChangePassword = event => {
+        setPassword(event.target.value)
+    }
+
+    const onChangeGender = event => {
+        setGender(event.target.value)
+    }
+
+    const onChangePhoneNumber = event => {
+        setPhoneNumber(event.target.value)
+    }
+
+    const onChangeAddress = event => {
+        setAddress(event.target.value)
+    }
+
+    const onChangeAge = event => {
+        setAge(event.target.value)
+    }
+
+
 
 
     return (
-        <Container maxWidth="xs" style={{border: "1px solid black"}}>
+        <Container maxWidth="xs" className="sign-up-container" style={{border: "1px solid black"}}>
             <CssBaseline />
-            {message && (
-                <div className="form-group">
-                    <div
-                        className={
-                            successful ? "alert alert-success" : "alert alert-danger"
-                        }
-                        role="alert"
-                    >
-                        {message}
-                    </div>
-                </div>
-            )}
+            {/*{message && (*/}
+            {/*    <div className="form-group">*/}
+            {/*        <div*/}
+            {/*            className={*/}
+            {/*                successful ? "alert alert-success" : "alert alert-danger"*/}
+            {/*            }*/}
+            {/*            role="alert"*/}
+            {/*        >*/}
+            {/*            {message}*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*)}*/}
             <div className={classes.paper}>
                 <Avatar className={classes.avatar}>
                     <LockOutlinedIcon />
@@ -161,162 +194,6 @@ const Register = () => {
                 <Typography component="h1" variant="h5">
                     Sign up
                 </Typography>
-                {/*<form className={classes.form} onSubmit={submitForm} ref={form}>*/}
-                    {/*<Input />*/}
-                    {/*<Grid container spacing={2}>*/}
-                    {/*    <Grid item xs={12} sm={6}>*/}
-                    {/*        <TextField*/}
-                    {/*            // autoComplete="fname"*/}
-                    {/*            name="firstName"*/}
-                    {/*            variant="outlined"*/}
-                    {/*            validations={[required, nameValidation]}*/}
-                    {/*            fullWidth*/}
-                    {/*            className="form-control"*/}
-                    {/*            value={firstName}*/}
-                    {/*            onChange={onChangeFirstName}*/}
-                    {/*            // id="firstName"*/}
-                    {/*            label="First Name"*/}
-                    {/*        />*/}
-                    {/*    </Grid>*/}
-                    {/*    <Grid item xs={12} sm={6}>*/}
-                    {/*        <TextField*/}
-                    {/*            variant="outlined"*/}
-                    {/*            // required*/}
-                    {/*            fullWidth*/}
-                    {/*            className="form-control"*/}
-
-                    {/*            // id="lastName"*/}
-                    {/*            validations={[required, nameValidation]}*/}
-                    {/*            label="Last Name"*/}
-                    {/*            name="lastName"*/}
-                    {/*            // autoComplete="lname"*/}
-                    {/*        />*/}
-                    {/*    </Grid>*/}
-                    {/*    <Grid item xs={12}>*/}
-                    {/*        <TextField*/}
-                    {/*            variant="outlined"*/}
-                    {/*            // required*/}
-                    {/*            fullWidth*/}
-                    {/*            // id="username"*/}
-                    {/*            className="form-control"*/}
-
-                    {/*            validations={[required, validUsername]}*/}
-                    {/*            label="Username"*/}
-                    {/*            name="username"*/}
-                    {/*            autoComplete="uname"*/}
-                    {/*        />*/}
-                    {/*    </Grid>*/}
-                    {/*    <Grid item xs={12}>*/}
-                    {/*        <TextField*/}
-                    {/*            variant="outlined"*/}
-                    {/*            // required*/}
-                    {/*            fullWidth*/}
-                    {/*            className="form-control"*/}
-
-                    {/*            // id="email"*/}
-                    {/*            validations={[validEmail, required]}*/}
-                    {/*            label="Email Address"*/}
-                    {/*            name="email"*/}
-                    {/*        />*/}
-                    {/*    </Grid>*/}
-                    {/*    <Grid item xs={12}>*/}
-                    {/*        <TextField*/}
-                    {/*            variant="outlined"*/}
-                    {/*            required*/}
-                    {/*            fullWidth*/}
-                    {/*            name="password"*/}
-                    {/*            label="Password"*/}
-                    {/*            type="password"*/}
-                    {/*            className="form-control"*/}
-
-                    {/*            validations={[validPassword, required]}*/}
-                    {/*            // id="password"*/}
-                    {/*            autoComplete="current-password"*/}
-                    {/*        />*/}
-                    {/*    </Grid>*/}
-                    {/*    <Grid item xs={12} sm={6}>*/}
-                    {/*        <TextField*/}
-                    {/*            variant="outlined"*/}
-                    {/*            // required*/}
-                    {/*            fullWidth*/}
-                    {/*            id="address"*/}
-                    {/*            label="Address"*/}
-                    {/*            className="form-control"*/}
-
-                    {/*            validations={[required]}*/}
-                    {/*            name="address"*/}
-                    {/*            // autoComplete="address"*/}
-                    {/*        />*/}
-                    {/*    </Grid>*/}
-                    {/*    <Grid item xs={12} sm={6}>*/}
-                    {/*        <TextField*/}
-                    {/*            variant="outlined"*/}
-                    {/*            // required*/}
-                    {/*            fullWidth*/}
-                    {/*            id="phoneNumber"*/}
-                    {/*            label="Phone number"*/}
-                    {/*            className="form-control"*/}
-
-                    {/*            validations={[validPhoneNumber, required]}*/}
-                    {/*            name="phoneNumber"*/}
-                    {/*            // autoComplete="pnumber"*/}
-                    {/*        />*/}
-                    {/*    </Grid>*/}
-                    {/*    <Grid item xs={12} sm={6}>*/}
-                    {/*        /!*<InputLabel id="demo-simple-select-filled-label">Age</InputLabel>*!/*/}
-                    {/*        <Select*/}
-                    {/*            // labelId="demo-simple-select-filled-label"*/}
-                    {/*            // id="demo-simple-select-filled"*/}
-                    {/*            variant="outlined"*/}
-                    {/*            fullWidth*/}
-                    {/*            label="Gender"*/}
-                    {/*            name="gender"*/}
-                    {/*            validations={[required]}*/}
-                    {/*            // value={age}*/}
-                    {/*            className="form-control"*/}
-
-                    {/*            // onChange={handleChange}*/}
-                    {/*        >*/}
-                    {/*            <MenuItem value="">*/}
-                    {/*                <em>None</em>*/}
-                    {/*            </MenuItem>*/}
-                    {/*            <MenuItem value="Male">Male</MenuItem>*/}
-                    {/*            <MenuItem value="Female">Female</MenuItem>*/}
-                    {/*        </Select>*/}
-                    {/*    </Grid>*/}
-                    {/*    <Grid item xs={12} sm={6}>*/}
-                    {/*        <TextField*/}
-                    {/*            variant="outlined"*/}
-                    {/*            className="form-control"*/}
-
-                    {/*            // required*/}
-                    {/*            type="number"*/}
-                    {/*            fullWidth*/}
-                    {/*            // id="age"*/}
-                    {/*            validations={[required, validAge]}*/}
-                    {/*            label="Age"*/}
-                    {/*            name="age"*/}
-                    {/*            // autoComplete="age"*/}
-                    {/*        />*/}
-                    {/*    </Grid>*/}
-                    {/*</Grid>*/}
-                {/*    <Button*/}
-                {/*        type="submit"*/}
-                {/*        fullWidth*/}
-                {/*        variant="contained"*/}
-                {/*        color="primary"*/}
-                {/*        className={classes.submit}*/}
-                {/*    >*/}
-                {/*        Sign Up*/}
-                {/*    </Button>*/}
-                {/*    <Grid container justifyContent="flex-end">*/}
-                {/*        <Grid item>*/}
-                {/*            <Link href="/login" variant="body2">*/}
-                {/*                Already have an account? Sign in*/}
-                {/*            </Link>*/}
-                {/*        </Grid>*/}
-                {/*    </Grid>*/}
-                {/*</form>*/}
                 {message && (
                     <div className="form-group">
                         <div
@@ -329,24 +206,40 @@ const Register = () => {
                         </div>
                     </div>
                 )}
-                {/*<div className="card card-container">*/}
-                {/*    <img*/}
-                {/*        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"*/}
-                {/*        alt="profile-img"*/}
-                {/*        className="profile-img-card"*/}
-                {/*    />*/}
                     <br/>
-                    <Form ref={form}>
+                    <Form onSubmit={submitForm} ref={form}>
                         <div>
+                            <div className="form-group">
+                                <label htmlFor="firstName">First name</label>
+                                <Input
+                                    type="text"
+                                    className="form-control"
+                                    name="firstName"
+                                    value={firstName}
+                                    onChange={onChangeFirstName}
+                                    validations={[required, nameValidation]}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="username">Last name</label>
+                                <Input
+                                    type="text"
+                                    className="form-control"
+                                    name="lastName"
+                                    value={lastName}
+                                    onChange={onChangeLastName}
+                                    validations={[required, nameValidation]}
+                                />
+                            </div>
                             <div className="form-group">
                                 <label htmlFor="username">Username</label>
                                 <Input
                                     type="text"
                                     className="form-control"
                                     name="username"
-                                    // value={username}
-                                    // onChange={onChangeUsername}
-                                    // validations={[required, vusername]}
+                                    value={username}
+                                    onChange={onChangeUsername}
+                                    validations={[required, validUsername]}
                                 />
                             </div>
 
@@ -357,7 +250,7 @@ const Register = () => {
                                     className="form-control"
                                     name="email"
                                     value={email}
-                                    // onChange={onChangeEmail}
+                                    onChange={onChangeEmail}
                                     validations={[required, validEmail]}
                                 />
                             </div>
@@ -369,7 +262,7 @@ const Register = () => {
                                     className="form-control"
                                     name="password"
                                     value={password}
-                                    // onChange={onChangePassword}
+                                    onChange={onChangePassword}
                                     validations={[required, validPassword]}
                                 />
                             </div>
@@ -379,20 +272,56 @@ const Register = () => {
                                 <Select
                                     className="form-control"
                                     name="email"
+                                    value={gender}
+                                    onChange={onChangeGender}
+                                    validations={[required]}
                                 >
+                                    <option value="" selected disabled hidden>Choose gender</option>
                                     <option value="Male">Male</option>
-                                    <option value="Female">Male</option>
+                                    <option value="Female">Female</option>
                                 </Select>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="phoneNumber">Phone number</label>
+                                <Input
+                                    type="number"
+                                    className="form-control"
+                                    name="phoneNumber"
+                                    value={phoneNumber}
+                                    onChange={onChangePhoneNumber}
+                                    validations={[required, validPhoneNumber]}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="Address">Address</label>
+                                <Input
+                                    type="text"
+                                    className="form-control"
+                                    name="address"
+                                    value={address}
+                                    onChange={onChangeAddress}
+                                    validations={[required]}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="age">Age</label>
+                                <Input
+                                    type="number"
+                                    className="form-control"
+                                    name="age"
+                                    value={age}
+                                    onChange={onChangeAge}
+                                    validations={[required, validAge]}
+                                />
                             </div>
                             <br/>
                             <div className="form-group">
                                 <Button type="submit" variant="contained" color="primary" block style={{margin: "10px"}}>Sign Up</Button>
-                                <Button variant="outline-info" variant="contained" color="success" block href="/login">Back to login</Button>
+                                <Button variant="contained" color="success" block href="/login">Back to login</Button>
                             </div>
                         </div>
                         <CheckButton style={{ display: "none" }} ref={checkBtn} />
                     </Form>
-                {/*</div>*/}
             </div>
         </Container>
     );
