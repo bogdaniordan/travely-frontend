cd2c4788c5c1eeedc25a94981b58ec1490e1eec0
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-11-10 15:12:18 +0200

    Added post notifications endpoints

--- src/service/HostService.js
+++ src/service/HostService.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import AuthHeader from "./auth-helpers/AuthHeader";
-import AuthService from "./AuthService";
 
 const HOST_SERVICE_API_URL = "http://localhost:8080/hosts";
 
